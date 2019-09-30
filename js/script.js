3746f74cb0507db7046041a8e7e45fce558b5161
Jakub Kiełbasa <jakub.kiel@gmail.com>
2019-09-30 17:50:10 +0200

    Add code in script.js to dd class .active to correct article

--- js/script.js
+++ js/script.js
@@ -23,14 +23,12 @@ const titleClickHandler = function (event) {
     console.log('articleSelector');
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /*[IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
-    const links = document.querySelectorAll('.titles a');
-
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
-    }
+    /*[DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
